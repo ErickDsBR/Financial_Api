88257c7c4c77e6636d26fce10cce0ced61d23919
ErickDsBR <erick.brasil2102@gmail.com>
2026-05-16 01:15:37 -0300

    feat(auth): create User entity with TypeORM

---
+++ src/auth/Entities/user_entities.ts
@@ -0,0 +1,13 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  readonly id!: number;
+  @Column()
+  name!: string;
+  @Column()
+  email!: string;
+  @Column()
+  password!: string;
+}
