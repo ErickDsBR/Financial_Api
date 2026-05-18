461c4b33de72f980fac8f547a608713aa7f44938
ErickDsBR <erick.brasil2102@gmail.com>
2026-05-17 22:06:27 -0300

    feat(auth): implement login with bcrypt validation and handle duplicate email error

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
-import { RegisterCredentials } from './dto/user.dtos';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { LoginCredentials, RegisterCredentials } from './dto/user.dtos';
 
 import { User } from './Entities/user_entities';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -14,28 +15,54 @@ export class AuthService {
 
   async register(credentials: RegisterCredentials) {
     const { email, name, password, confirmPassword } = credentials;
+    try {
+      if (password !== confirmPassword) {
+        throw new Error('Password and confirm password do not match');
+      }
 
-    if (password !== confirmPassword) {
-      throw new Error('Password and confirm password do not match');
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      const newUser = {
+        email,
+        name,
+        password: hashedPassword,
+      };
+
+      this.userRepository.create(newUser);
+      await this.userRepository.save(newUser);
+
+      return {
+        message: 'User registered successfully',
+        user: { email, name },
+      };
+    } catch (error: any) {
+      if (error.code === '23505') {
+        throw new ConflictException('Email already exists');
+      }
+
+      throw error;
     }
+  }
+  async login(credentials: LoginCredentials) {
+    const user = await this.userRepository.findOne({
+      where: { email: credentials.email },
+    });
 
-    const userExists = await this.userRepository.findOne({ where: { email } });
-    if (userExists) {
-      throw new Error('User with this email already exists');
+    if (!user) {
+      throw new Error('Invalid email or password');
     }
+    const isPasswordValid = await bcrypt.compare(
+      credentials.password,
+      user.password,
+    );
 
-    const newUser = {
-      email,
-      name,
-      password,
-    };
-
-    this.userRepository.create(newUser);
-    await this.userRepository.save(newUser);
+    if (!isPasswordValid) {
+      throw new Error('Invalid email or password');
+    }
 
     return {
-      message: 'User registered successfully',
-      user: { email, name },
+      message: 'Login successful',
+      user: { email: user.email },
     };
   }
 }
