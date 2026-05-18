46e275c0a425346e78d9aeb6c3fef6a467e3dc76
ErickDsBR <erick.brasil2102@gmail.com>
2026-05-17 22:06:20 -0300

    feat(auth): add login endpoint with LoginCredentials DTO

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { RegisterCredentials } from './dto/user.dtos';
+import { LoginCredentials, RegisterCredentials } from './dto/user.dtos';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +9,9 @@ export class AuthController {
   @Post('/register')
   register(@Body() credentials: RegisterCredentials) {
     return this.authService.register(credentials);
+  }
+  @Post('/login')
+  login(@Body() credentials: LoginCredentials) {
+    return this.authService.login(credentials);
   }
 }
