f28429b1c9b0c9c71a8540758ad4dd6978360b04
ErickDsBR <erick.brasil2102@gmail.com>
2026-07-11 11:40:10 -0300

    chore: update tsconfig, eslint, and gitignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -33,7 +33,7 @@ export default tseslint.config(
       '@typescript-eslint/no-unsafe-member-access': 'off',
       '@typescript-eslint/no-unsafe-return': 'off',
       '@typescript-eslint/no-unsafe-assignment': 'off',
-      'prettier/prettier': ['off', { endOfLine: 'auto' }],
+      "prettier/prettier": ["error", { endOfLine: "auto" }],
     },
   },
 );
