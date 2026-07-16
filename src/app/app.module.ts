deac470d934b2c3230c6ef5c9f99fc3e7bb8e985
ErickDsBR <erick.brasil2102@gmail.com>
2026-07-16 13:56:01 -0300

    fix: corrige import do DatabaseModule para caminho relativo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from "@nestjs/common";
 import { AuthModule } from "../modules/auth/auth.module";
-import { DatabaseModule } from "src/database/database.module";
+import { DatabaseModule } from "../database/database.module";
 
 @Module({
   imports: [AuthModule, DatabaseModule],
