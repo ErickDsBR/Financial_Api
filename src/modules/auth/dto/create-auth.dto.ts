90437282bb2d12eb053278e0ee332e85888b06a0
ErickDsBR <erick.brasil2102@gmail.com>
2026-07-17 12:31:27 -0300

    fix: adiciona decorators de validacao no DTO de auth

--- src/modules/auth/dto/create-auth.dto.ts
+++ src/modules/auth/dto/create-auth.dto.ts
@@ -1,5 +1,14 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
+
 export class CreateAuthDto {
+  @IsString()
+  @IsNotEmpty({ message: "O nome não pode estar vazio" })
   name!: string;
+
+  @IsEmail({}, { message: "O email deve ser válido" })
   email!: string;
+
+  @IsString()
+  @MinLength(6, { message: "A senha deve ter no mínimo 6 caracteres" })
   password!: string;
 }
